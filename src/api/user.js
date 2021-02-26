d69fcc1ff2243cc5fc8a6aff7792ee7cc9c4202e
陈童 <1421911991@qq.com>
2021-02-26 15:05:16 +0800

    展示当前的用户信息

--- src/api/user.js
+++ src/api/user.js
@@ -3,6 +3,7 @@
  */
 
 import request from '@/utils/request'
+import store from '@/store'
 
 export const login = data => {
   return request({
@@ -16,5 +17,16 @@ export const sendSms = mobile => {
   return request({
     method: 'GET',
     url: `/app/v1_0/sms/codes/${mobile}`
+  })
+}
+
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user',
+    // 携带请求头信息
+    headers: {
+      Authorization: `Bearer ${store.state.user.token}`
+    }
   })
 }
--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -4,13 +4,8 @@
     <div v-if="user" class="header user-info">
       <div class="base-info">
         <div class="left">
-          <van-image
-            class="avatar"
-            round
-            fit="cover"
-            src="https://img.yzcdn.cn/vant/cat.jpeg"
-          />
-          <span class="name">黑马头条号</span>
+          <van-image class="avatar" round fit="cover" :src="userInfo.photo" />
+          <span class="name">{{ userInfo.name }}</span>
         </div>
         <div class="right">
           <van-button size="mini" round>编辑资料</van-button>
@@ -18,20 +13,20 @@
       </div>
       <div class="data-stats">
         <div class="data-item">
-          <span class="count">10</span>
+          <span class="count">{{ userInfo.art_count }}</span>
           <span class="text">头条</span>
         </div>
         <div class="data-item">
-          <span class="count">10</span>
-          <span class="text">头条</span>
+          <span class="count">{{ userInfo.follow_count }}</span>
+          <span class="text">关注</span>
         </div>
         <div class="data-item">
-          <span class="count">10</span>
-          <span class="text">头条</span>
+          <span class="count">{{ userInfo.fans_count }}</span>
+          <span class="text">粉丝</span>
         </div>
         <div class="data-item">
-          <span class="count">10</span>
-          <span class="text">头条</span>
+          <span class="count">{{ userInfo.like_count }}</span>
+          <span class="text">获赞</span>
         </div>
       </div>
     </div>
@@ -81,15 +76,24 @@
 
 <script>
 import { mapState } from 'vuex'
+import { getUserInfo } from '@/api/user.js'
 export default {
   name: 'MyIndex',
 
   data() {
-    return {}
+    return {
+      userInfo: {}
+    }
   },
 
   computed: {
     ...mapState(['user'])
+  },
+
+  created() {
+    if (this.user) {
+      this.loadUserInfo()
+    }
   },
 
   methods: {
@@ -108,6 +112,15 @@ export default {
         .catch(() => {
           // on cancel
         })
+    },
+
+    async loadUserInfo() {
+      try {
+        const { data } = await getUserInfo()
+        this.userInfo = data.data
+      } catch (err) {
+        this.$toast('获取数据失败，请稍后重试')
+      }
     }
   }
 }
