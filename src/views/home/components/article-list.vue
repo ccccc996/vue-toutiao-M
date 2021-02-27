6ec3191d147d9ec97372571cb362dee5b0d1d999
陈童 <1421911991@qq.com>
2021-02-27 14:35:23 +0800

    请求频道数据并处理

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -6,12 +6,18 @@
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <van-cell
+        v-for="(article, index) in list"
+        :key="index"
+        :title="article.title"
+      />
     </van-list>
   </div>
 </template>
 
 <script>
+import { getArticle } from '@/api/article'
+
 export default {
   name: 'ArticleList',
 
@@ -26,33 +32,37 @@ export default {
     return {
       list: [], // 存储列表数据的数组
       loading: false, // 控制加载中 loading 状态
-      finished: false // 控制数据加载结束的状态
+      finished: false, // 控制数据加载结束的状态
+      timestamp: null
     }
   },
 
   methods: {
     // 初始化或滚动到底部的时候会触发调用 onLoad
-    onLoad() {
-      console.log('onLoad')
-      // 1. 请求获取数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
+    async onLoad() {
+      try {
+        // 1. 请求获取数据
+        const { data } = await getArticle({
+          channel_id: this.channel.id, // 频道 id
+          timestamp: this.timestamp || Date.now(), // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳
+          with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+        })
+        console.log(data)
         // 2. 把请求结果数据放到 list 数组中
-        for (let i = 0; i < 10; i++) {
-          // 0 + 1 = 1
-          this.list.push(this.list.length + 1)
-        }
-
+        const { results } = data.data
+        this.list.push(...results)
         // 3. 本次数据加载结束之后要把加载状态设置为结束
-        //     loading 关闭以后才能触发下一次的加载更多
         this.loading = false
-
+        // loading 关闭以后才能触发下一次的加载更多
         // 4. 判断数据是否全部加载完成
-        if (this.list.length >= 40) {
+        if (results.length) {
+          // 更新获取下一页数据的时间戳
+          this.timestamp = data.data.pre_timestamp
+        } else {
           // 如果没有数据了，把 finished 设置为 true，之后不再触发加载更多
           this.finished = true
         }
-      }, 1000)
+      } catch (err) {}
     }
   }
 }
