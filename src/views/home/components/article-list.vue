96a68dfd9afa4e320310f5fa1e7c4be348bf0c33
陈童 <1421911991@qq.com>
2021-02-27 16:41:28 +0800

    下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="article-list">
-    <van-list
-      v-model="loading"
-      :finished="finished"
-      finished-text="没有更多了"
-      :error.sync="error"
-      error-text="请求失败，点击重新加载"
-      @load="onLoad"
+    <van-pull-refresh
+      v-model="isRefreshLoading"
+      @refresh="onRefresh"
+      success-text="refreshSuccessText"
+      success-duration="1500"
     >
-      <van-cell
-        v-for="(article, index) in list"
-        :key="index"
-        :title="article.title"
-      />
-    </van-list>
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        :error.sync="error"
+        error-text="请求失败，点击重新加载"
+        @load="onLoad"
+      >
+        <van-cell
+          v-for="(article, index) in list"
+          :key="index"
+          :title="article.title"
+        />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -36,7 +43,9 @@ export default {
       loading: false, // 控制加载中 loading 状态
       finished: false, // 控制数据加载结束的状态
       timestamp: null, // 请求下一页数据的时间戳
-      error: false // 是否加载失败
+      error: false, // 是否加载失败
+      isRefreshLoading: false, // 控制下拉刷新的 loading 状态
+      refreshSuccessText: '刷新成功'
     }
   },
 
@@ -51,9 +60,9 @@ export default {
           with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
         })
         // 模拟随机失败的状态
-        if (Math.random() > 0.5) {
-          JSON.parse('sdgjhgsdajhgds')
-        }
+        // if (Math.random() > 0.5) {
+        //   JSON.parse('sdgjhgsdajhgds')
+        // }
         // 2. 把请求结果数据放到 list 数组中
         const { results } = data.data
         this.list.push(...results)
@@ -72,6 +81,33 @@ export default {
         console.log(err)
         this.loading = false // 关闭 loading 效果
         this.error = true // 开启错误提示
+      }
+    },
+
+    async onRefresh() {
+      try {
+        // 1. 请求获取数据
+        const { data } = await getArticle({
+          channel_id: this.channel.id,
+          timestamp: Date.now(),
+          with_top: 1
+        })
+        // 模拟随机失败的状态
+        // if (Math.random() > 0.5) {
+        //   JSON.parse('sdgjhgsdajhgds')
+        // }
+        // 2. 将数据追加到列表的顶部
+        const { results } = data.data
+        this.list.unshift(...results)
+        // 3. 关闭下拉刷新的 loading 状态
+        this.isRefreshLoading = false
+        // 4. 更新下拉刷新成功提示的文本
+        this.refreshSuccessText = `刷新成功，更新了${results.length}条数据`
+      } catch (err) {
+        // 关闭下拉刷新的 loading 状态
+        this.isRefreshLoading = false
+        // 更新下拉刷新成功提示的文本
+        this.refreshSuccessText = '刷新失败'
       }
     }
   }
