98190934dc816db6dbdfd92f2c03dbe1e987b5da
陈童 <1421911991@qq.com>
2021-03-02 15:19:16 +0800

    点击取消返回

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="search-container">
-    搜索页面
+    <form action="/">
+      <van-search
+        v-model="value"
+        show-action
+        background="#3296fa"
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
   </div>
 </template>
 
@@ -9,11 +18,29 @@ export default {
   name: 'searchIndex',
 
   data() {
-    return {}
+    return {
+      value: ''
+    }
   },
 
-  methods: {}
+  methods: {
+    onSearch(val) {
+      console.log(val)
+    },
+    onCancel() {
+      this.$router.back()
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.search-container {
+  .van-search__action {
+    color: #fff;
+  }
+}
+.van-search__action:active {
+  background-color: transparent;
+}
+</style>
