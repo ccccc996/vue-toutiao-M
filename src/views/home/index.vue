9e4e0da1e22c42aaf2cb1f375a1b46f4a816413c
陈童 <1421911991@qq.com>
2021-03-02 14:46:48 +0800

    首页频道列表展示逻辑

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -59,6 +59,8 @@
 import { getUserChannels } from '@/api/user'
 import ArticleList from './components/article-list'
 import ChannelEdit from './components/channel-edit'
+import { mapState } from 'vuex'
+import { getItem } from '@/utils/storage'
 
 export default {
   name: 'HomeIndex',
@@ -75,6 +77,10 @@ export default {
     this.loadChannels()
   },
 
+  computed: {
+    ...mapState(['user'])
+  },
+
   components: {
     ArticleList,
     ChannelEdit
@@ -83,8 +89,24 @@ export default {
   methods: {
     async loadChannels() {
       try {
-        const { data } = await getUserChannels()
-        this.channels = data.data.channels
+        let channels = []
+        // 如果登陆了
+        if (this.user) {
+          const { data } = await getUserChannels()
+          channels = data.data.channels
+        } else {
+          // 未登录
+          const localChannels = getItem('TOUTIAO_CHANNELS')
+          if (localChannels) {
+            // 本地有，直接用
+            channels = localChannels
+          } else {
+            // 本地没有，未登录则会获取默认频道列表
+            const { data } = await getUserChannels()
+            channels = data.data.channels
+          }
+        }
+        this.channels = channels
       } catch (err) {
         this.$toast('获取用户频道失败')
       }
