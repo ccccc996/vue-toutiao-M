3f49d8f4954dbb63f7d111378ed8599ace8840a3
陈童 <1421911991@qq.com>
2021-03-02 11:17:54 +0800

    登陆状态下添加频道到后端

--- src/api/channel.js
+++ src/api/channel.js
@@ -2,9 +2,25 @@
 
 import request from '@/utils/request'
 
+/**
+ * 获取所有频道
+ */
 export const getAllChannels = () => {
   return request({
     method: 'GET',
     url: '/app/v1_0/channels'
+  })
+}
+
+/**
+ * 添加用户频道
+ */
+export const addUserChannel = channel => {
+  return request({
+    method: 'PATCH',
+    url: '/app/v1_0/user/channels',
+    data: {
+      channels: [channel]
+    }
   })
 }
