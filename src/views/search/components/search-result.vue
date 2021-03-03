73129cfacf99db4f7b97e4259e6b4d315dd4f5c5
陈童 <1421911991@qq.com>
2021-03-03 11:14:18 +0800

    列表处理

--- src/views/search/components/search-result.vue
+++ src/views/search/components/search-result.vue
@@ -5,13 +5,21 @@
       :finished="finished"
       finished-text="没有更多了"
       @load="onLoad"
+      :error.sync="error"
+      error-text="加载失败，请点击重试"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <van-cell
+        v-for="(article, index) in list"
+        :key="index"
+        :title="article.title"
+      />
     </van-list>
   </div>
 </template>
 
 <script>
+import { getSearchResult } from '@/api/search'
+
 export default {
   name: 'SearchResult',
 
@@ -26,25 +34,43 @@ export default {
     return {
       list: [],
       loading: false,
-      finished: false
+      finished: false,
+      page: 1,
+      per_page: 20
     }
   },
 
   methods: {
-    onLoad() {
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
+    async onLoad() {
+      try {
+        // 1. 请求数据
+        const { data } = await getSearchResult({
+          page: this.page, // 页码
+          per_page: this.per_page, // 每页条数
+          q: this.searchText // 查询关键词
+        })
+
+        // console.log(data)
+        // 2. 将数据添加到数组列表中
+        const { results } = data.data
+        this.list.push(...results)
+
+        // 3. 将本次加载中的 loading 关闭
         this.loading = false
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
+
+        // 4. 判断是否还有数据
+        if (results.length) {
+          // 4.1 如果有，则更新获取下一个数据的页码
+          this.page++
+        } else {
+          // 4.2 如果无，则将加载状态 finished 设置为结束
           this.finished = true
         }
-      }, 1000)
+      } catch (err) {
+        this.error = true // 展示加载失败
+        this.loading = false // 关闭 loading
+        // console.log('请求数据失败')
+      }
     }
   }
 }
