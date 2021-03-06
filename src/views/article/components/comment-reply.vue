8c6de04e3f9bbd54d24b02928be983419942d2db
陈童 <1421911991@qq.com>
2021-03-06 21:31:18 +0800

    展示回复列表基本处理

--- src/views/article/components/comment-reply.vue
+++ src/views/article/components/comment-reply.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="comment-reply">
-    <van-nav-bar title="xx条回复">
+    <van-nav-bar
+      :title="
+        comment.reply_count > 0 ? `${comment.reply_count}条回复` : '暂无回复'
+      "
+    >
       <van-icon slot="left" name="cross" @click="$emit('close')" />
     </van-nav-bar>
     <!-- 当前评论项 -->
     <comment-item :comment="comment" />
+    <!-- 评论回复列表 -->
+    <van-cell title="全部回复" />
+    <comment-list type="c" :source="comment.com_id" />
   </div>
 </template>
 
 <script>
 import commentItem from './comment-item'
+import commentList from './comment-list'
 
 export default {
   name: 'CommentReply',
@@ -22,7 +30,8 @@ export default {
   },
 
   components: {
-    commentItem
+    commentItem,
+    commentList
   },
 
   data() {
