af80d8a462c0881ef364adb9505a38bc8e8b0a71
陈童 <1421911991@qq.com>
2021-03-06 19:20:16 +0800

    评论为空时的处理

--- src/views/article/components/comment-post.vue
+++ src/views/article/components/comment-post.vue
@@ -2,7 +2,7 @@
   <div class="comment-post">
     <van-field
       class="post-field"
-      v-model="message"
+      v-model.trim="message"
       rows="2"
       autosize
       type="textarea"
@@ -10,7 +10,9 @@
       placeholder="请输入留言"
       show-word-limit
     />
-    <van-button class="post-btn" @click="onPost">发布</van-button>
+    <van-button class="post-btn" @click="onPost" :disabled="!message.length"
+      >发布</van-button
+    >
   </div>
 </template>
 
@@ -35,6 +37,12 @@ export default {
 
   methods: {
     async onPost() {
+      // 自定义加载图标
+      this.$toast.loading({
+        message: '加载中...',
+        forbidClick: true,
+        duration: 0
+      })
       try {
         const { data } = await addComment({
           target: this.target, // 评论的目标 ID（评论文章即为文章ID，对评论进行回复则为评论ID）
@@ -46,7 +54,8 @@ export default {
         // 将评论的内容添加到list列表中
         this.message = ''
         this.$emit('post-success', data.data)
-        console.log(data)
+        this.$toast.success('发布成功')
+        // console.log(data)
       } catch (err) {
         this.$toast.fail('发布失败')
       }
