e67759d87c5e3ee5179d9112fdab424af89f8d98
陈童 <1421911991@qq.com>
2021-03-06 19:30:52 +0800

    准备评论回复的弹出层

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -99,7 +99,10 @@
         <!-- /底部区域 -->
         <!-- 发布评论的弹层 -->
         <van-popup v-model="isPostShow" position="bottom">
-          <comment-post :target="article.art_id" @post-success="onPostSuccess"></comment-post>
+          <comment-post
+            :target="article.art_id"
+            @post-success="onPostSuccess"
+          ></comment-post>
         </van-popup>
       </div>
       <!-- /加载完成-文章详情 -->
@@ -119,6 +122,11 @@
       </div>
       <!-- /加载失败：其它未知错误（例如网络原因或服务端异常） -->
     </div>
+
+    <!-- 评论回复 -->
+    <van-popup v-model="isReplyShow" position="bottom" :style="{ height: '100%' }"
+      >hello</van-popup
+    >
   </div>
 </template>
 
@@ -151,7 +159,8 @@ export default {
       followLoading: false, // 关注按钮的 loading 状态
       totalCommentCount: 0,
       isPostShow: false, // 控制评论弹层显示/隐藏
-      commentList: [] // 评论列表
+      commentList: [], // 评论列表
+      isReplyShow: false
     }
   },
 
