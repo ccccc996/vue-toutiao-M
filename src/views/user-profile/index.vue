61d48bff26755048acf680faa8d530d9e7b143a3
陈童 <1421911991@qq.com>
2021-03-07 11:29:12 +0800

    页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="user-profile">
-    编辑资料
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="page-nav-bar"
+      title="个人信息"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- 单元格 -->
+    <van-cell title="头像" value="内容" is-link>
+      <van-image
+        class="avatar"
+        fit="cover"
+        round
+        src="https://img01.yzcdn.cn/vant/cat.jpeg"
+      />
+    </van-cell>
+    <van-cell title="昵称" value="内容" is-link />
+    <van-cell title="性别" value="内容" is-link />
+    <van-cell title="生日" value="内容" is-link />
   </div>
 </template>
 
@@ -16,4 +34,14 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.user-profile {
+  .avatar {
+    width: 60px;
+    height: 60px;
+  }
+  .van-popup {
+    background-color: #f5f7f9;
+  }
+}
+</style>
