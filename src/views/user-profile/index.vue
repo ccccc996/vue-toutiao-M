fe37a8a5e8a67465cbc8f31dab829893230ef689
陈童 <1421911991@qq.com>
2021-03-07 11:36:55 +0800

    展示用户信息

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -9,28 +9,40 @@
     />
     <!-- 单元格 -->
     <van-cell title="头像" value="内容" is-link>
-      <van-image
-        class="avatar"
-        fit="cover"
-        round
-        src="https://img01.yzcdn.cn/vant/cat.jpeg"
-      />
+      <van-image class="avatar" fit="cover" round :src="user.photo" />
     </van-cell>
-    <van-cell title="昵称" value="内容" is-link />
-    <van-cell title="性别" value="内容" is-link />
-    <van-cell title="生日" value="内容" is-link />
+    <van-cell title="昵称" :value="user.name" is-link />
+    <van-cell title="性别" :value="user.gender === 0 ? '男' : '女'" is-link />
+    <van-cell title="生日" :value="user.birthday" is-link />
   </div>
 </template>
 
 <script>
+import { getUserProfile } from '@/api/user'
+
 export default {
   name: 'UserProfile',
 
   data() {
-    return {}
+    return {
+      user: {} // 个人信息
+    }
   },
 
-  methods: {}
+  created() {
+    this.loadUserProfile()
+  },
+
+  methods: {
+    async loadUserProfile() {
+      try {
+        const { data } = await getUserProfile()
+        this.user = data.data
+      } catch (err) {
+        this.$toast('数据获取失败')
+      }
+    }
+  }
 }
 </script>
 
