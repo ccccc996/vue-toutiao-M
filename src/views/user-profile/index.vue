17853a6f5ee8b0fb7d6789ab4b87be3ac9286d15
陈童 <1421911991@qq.com>
2021-03-07 10:39:09 +0800

    创建组件并配置路由

---
+++ src/views/user-profile/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user-profile">
+    编辑资料
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
