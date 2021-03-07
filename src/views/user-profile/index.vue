2aaddd7c8e1f89fb994fc2931dfda80cd78ba7f9
陈童 <1421911991@qq.com>
2021-03-07 11:56:04 +0800

    展示修改昵称弹出层

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -11,9 +11,21 @@
     <van-cell title="头像" value="内容" is-link>
       <van-image class="avatar" fit="cover" round :src="user.photo" />
     </van-cell>
-    <van-cell title="昵称" :value="user.name" is-link />
+    <van-cell
+      title="昵称"
+      :value="user.name"
+      is-link
+      @click="isUpdateNameShow = true"
+    />
     <van-cell title="性别" :value="user.gender === 0 ? '男' : '女'" is-link />
     <van-cell title="生日" :value="user.birthday" is-link />
+    <!-- 编辑昵称 -->
+    <van-popup
+      v-model="isUpdateNameShow"
+      position="bottom"
+      :style="{ height: '100%' }"
+      >hello</van-popup
+    >
   </div>
 </template>
 
@@ -25,7 +37,8 @@ export default {
 
   data() {
     return {
-      user: {} // 个人信息
+      user: {}, // 个人信息
+      isUpdateNameShow: false
     }
   },
 
